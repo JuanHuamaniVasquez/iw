2912f65d56bfef9036f3c10661be214b55d230b6
Sam <selenamoralesdiaz1@gmail.com>
2026-06-11 19:43:25 -0500

    practica

---
+++ vue-project/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import EnrollmentCertificate from './components/EnrollmentCertificate.vue'
+</script>
+
+<template>
+  <EnrollmentCertificate />
+</template>
+
+<style>
+body {
+  margin: 0;
+  background-color: #f5f5f5;
+}
+</style>
